996a2c4cf8d05290e58bdab1ca10e860c05cd25b
luluw <shashinmhrzn@gmail.com>
2024-03-21 21:04:24 +0545

    :zap: Inference Changes for CTC Decoder

--- decoder.py
+++ decoder.py
@@ -2,41 +2,52 @@ from neuralnet.utils import TextProcess
 import torch
 import ctcdecode
 
+# Initialize TextProcess for text processing
 textprocess = TextProcess()
 
-labels = [
-    "'",  # 0
-    " ",  # 1
-    "a",  # 2
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",  # 27
-    "_",  # 28, blank
-]
+# Define labels for the vocabulary
+labels = ["'",  # 0
+          " ",  # 1
+          "a",  # 2
+          "b",
+          "c",
+          "d",
+          "e",
+          "f",
+          "g",
+          "h",
+          "i",
+          "j",
+          "k",
+          "l",
+          "m",
+          "n",
+          "o",
+          "p",
+          "q",
+          "r",
+          "s",
+          "t",
+          "u",
+          "v",
+          "w",
+          "x",
+          "y",
+          "z",  # 27
+          "_"]  # 28, blank
 
 def DecodeGreedy(output, blank_label=28, collapse_repeated=True):
+    """
+    Decodes the output of the neural network using greedy decoding.
+
+    Args:
+        output (torch.Tensor): Output tensor from the neural network.
+        blank_label (int): Index of the blank label in the vocabulary.
+        collapse_repeated (bool): Whether to collapse repeated characters.
+
+    Returns:
+        str: Decoded text sequence.
+    """
 	arg_maxes = torch.argmax(output, dim=2).squeeze(1)
 	decode = []
 	for i, index in enumerate(arg_maxes):
@@ -47,8 +58,26 @@ def DecodeGreedy(output, blank_label=28, collapse_repeated=True):
 	return textprocess.int_to_text_sequence(decode)
 
 class CTCBeamDecoder:
+    """
+    Class for performing CTC beam decoding with a language model.
+
+    Args:
+        beam_size (int): Beam size for beam search decoding.
+        blank_id (int): Index of the blank label in the vocabulary.
+        kenlm_path (str): Path to the KenLM language model file.
+    """
 
     def __init__(self, beam_size=100, blank_id=labels.index('_'), kenlm_path=None):
+        """
+        Initializes the CTCBeamDecoder.
+
+        Args:
+            beam_size (int): Beam size for beam search decoding.
+            blank_id (int): Index of the blank label in the vocabulary.
+            kenlm_path (str): Path to the KenLM language model file.
+            alpha (float): Scaling factor for language model score.
+            beta (float): Scaling factor for length normalization.
+        """
         print("loading beam search with lm...")
         self.decoder = ctcdecode.CTCBeamDecoder(
             labels, alpha=0.522729216841, beta=0.96506699808,
@@ -57,8 +86,30 @@ class CTCBeamDecoder:
         print("finished loading beam search")
 
     def __call__(self, output):
+        """
+        Perform beam search decoding on the given output.
+
+        Args:
+            output (torch.Tensor): Output tensor from the neural network.
+
+        Returns:
+            str: Decoded text sequence.
+        """
+        # Perform beam search decoding
         beam_result, beam_scores, timesteps, out_seq_len = self.decoder.decode(output)
+        # Convert result to string
         return self.convert_to_string(beam_result[0][0], labels, out_seq_len[0][0])
 
     def convert_to_string(self, tokens, vocab, seq_len):
+        """
+        Convert token sequence to string using the vocabulary.
+
+        Args:
+            tokens (list): List of token indices.
+            vocab (list): Vocabulary list.
+            seq_len (int): Length of the sequence.
+
+        Returns:
+            str: Decoded text sequence.
+        """
         return ''.join([vocab[x] for x in tokens[0:seq_len]])
--- engine.py
+++ engine.py
@@ -13,37 +13,80 @@ from threading import Event
 
 
 class Listener:
+    """
+    Class to handle audio input streaming using PyAudio.
+    Attributes:
+        listener (Listener): Instance of the Listener class for audio input streaming.
+        model (torch.jit.ScriptModule): Pre-trained model for speech recognition.
+        featurizer (function): Function to extract features from audio waveforms.
+        audio_q (list): List to store audio input data.
+        hidden (tuple): Tuple containing hidden states for the model.
+        beam_results (str): Result of the beam search decoding.
+        out_args (torch.Tensor): Output tensor from the model.
+        beam_search (CTCBeamDecoder): Instance of the CTCBeamDecoder class for beam search decoding.
+        context_length (int): Length of the context window in frames.
+        start (bool): Flag to indicate if the speech recognition engine has started.
+    """
 
     def __init__(self, sample_rate=8000, record_seconds=2):
+        """
+        Initializes the Listener object with specified parameters.
+
+        Args:
+            sample_rate (int): Sampling rate for audio input (default: 8000 Hz).
+            record_seconds (int): Duration of each audio recording in seconds (default: 2 seconds).
+        """
         self.chunk = 1024
         self.sample_rate = sample_rate
         self.record_seconds = record_seconds
         self.p = pyaudio.PyAudio()
         self.stream = self.p.open(format=pyaudio.paInt16,
-                        channels=1,
-                        rate=self.sample_rate,
-                        input=True,
-                        output=True,
-                        frames_per_buffer=self.chunk)
+                                  channels=1,
+                                  rate=self.sample_rate,
+                                  input=True,
+                                  output=True,
+                                  frames_per_buffer=self.chunk)
 
     def listen(self, queue):
+        """
+        Continuously listens for audio input and appends it to the provided queue.
+
+        Args:
+            queue (list): List to store audio input data.
+        """
         while True:
             data = self.stream.read(self.chunk , exception_on_overflow=False)
             queue.append(data)
             time.sleep(0.01)
 
     def run(self, queue):
+        """
+        Starts a new thread to listen for audio input.
+
+        Args:
+            queue (list): List to store audio input data.
+        """
         thread = threading.Thread(target=self.listen, args=(queue,), daemon=True)
         thread.start()
-        print("\Speech Recognition engine is now listening... \n")
+        print("\nSpeech Recognition engine is now listening... \n")
 
 
 class SpeechRecognitionEngine:
-
+    """
+    Class to perform speech recognition using a pre-trained model.
+    """
     def __init__(self, model_file, ken_lm_file, context_length=10):
+        """
+        Initializes the SpeechRecognitionEngine with the specified parameters.
+
+        Args:
+            model_file (str): Path to the optimized model file.
+            ken_lm_file (str): Path to the KenLM language model file.
+            context_length (int): Length of the context window in seconds (default: 10 seconds).
+        """
         self.listener = Listener(sample_rate=8000)
         self.model = torch.jit.load(model_file)
-        self.model.eval().to('cpu')  #run on cpu
+        self.model.eval().to('cpu')  # Run on cpu
         self.featurizer = get_featurizer(8000)
         self.audio_q = list()
         self.hidden = (torch.zeros(1, 1, 1024), torch.zeros(1, 1, 1024))
@@ -54,6 +97,16 @@ class SpeechRecognitionEngine:
         self.start = False
 
     def save(self, waveforms, fname="audio_temp"):
+        """
+        Saves audio waveforms to a WAV file.
+
+        Args:
+            waveforms (list): List of audio waveforms.
+            fname (str): File name for the saved WAV file (default: "audio_temp").
+
+        Returns:
+            str: File path of the saved WAV file.
+        """
         wf = wave.open(fname, "wb")
         # set the channels
         wf.setnchannels(1)
@@ -68,6 +121,15 @@ class SpeechRecognitionEngine:
         return fname
 
     def predict(self, audio):
+        """
+        Performs speech recognition on the provided audio data.
+
+        Args:
+            audio (list): List of audio waveforms.
+
+        Returns:
+            tuple: A tuple containing the recognized text and the current context length in seconds.
+        """
         with torch.inference_mode():
             fname = self.save(audio)
             waveform, _ = torchaudio.load(fname)  # don't normalize on train
@@ -83,6 +145,12 @@ class SpeechRecognitionEngine:
             return results, current_context_length
 
     def inference_loop(self, action):
+        """
+        Continuously performs speech recognition on the audio queue.
+
+        Args:
+            action (function): Function to perform after speech recognition.
+        """
         while True:
             if len(self.audio_q) < 5:
                 continue
@@ -93,6 +161,12 @@ class SpeechRecognitionEngine:
             time.sleep(0.05)
 
     def run(self, action):
+        """
+        Starts the speech recognition engine.
+
+        Args:
+            action (function): Function to perform after speech recognition.
+        """
         self.listener.run(self.audio_q)
         thread = threading.Thread(target=self.inference_loop,
                                     args=(action,), daemon=True)
@@ -100,16 +174,32 @@ class SpeechRecognitionEngine:
 
 
 class DemoAction:
-
+    """
+    Class to handle the action after performing speech recognition.
+    """
     def __init__(self):
         self.asr_results = ""
         self.current_beam = ""
 
     def __call__(self, x):
+        """
+        Combines the current and previous results and prints them.
+
+        Args:
+            x (tuple): A tuple containing the recognized text and the current context length.
+
+        Returns:
+            None
+        """
+        # Unpack the tuple
         results, current_context_length = x
+
+        # Combine the current beam results with previous results and print transcript
         self.current_beam = results
         transcript = " ".join(self.asr_results.split() + results.split())
         print(transcript)
+
+        # Update the current transcript
         if current_context_length > 10:
             self.asr_results = transcript
 
@@ -121,9 +211,12 @@ if __name__ == "__main__":
                         help='If you have an ngram lm use to decode')
 
     args = parser.parse_args()
+
+    # activate speech recognition engine
     asr_engine = SpeechRecognitionEngine(args.model_file, args.ken_lm_file)
     action = DemoAction()
 
+    # Start the speech recognition engine 
+    # and wait for the threading event to keep the program running
     asr_engine.run(action)
     threading.Event().wait()
-    # activate speech recognition engine
