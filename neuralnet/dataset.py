0dff51b3b3459a14172ec544fa728f70687f99e8
luluw <shashinmhrzn@gmail.com>
2024-09-28 10:55:32 +0545

    :rocket: [Update] Librispeech Supported

--- neuralnet/dataset.py
+++ neuralnet/dataset.py
@@ -1,215 +1,134 @@
-import torch
+import pytorch_lightning as pl
 import torchaudio
+import torch
 import torch.nn as nn
-import pandas as pd
-import numpy as np
-from utils import TextProcess  # Comment this before running engine.py after training
-
-
-# NOTE: add time stretch
-class SpecAugment(nn.Module):
-    """
-    Parameters:
-        - rate (float): The probability of applying SpecAugment.
-        - policy (int): The augmentation policy to use (1, 2, or 3).
-        - freq_mask (int): Maximum frequency masking parameter.
-        - time_mask (int): Maximum time masking parameter.
-
-    Methods:
-        - forward(x): Applies the specified augmentation policy to the input tensor x.
-
-    Augmentation Policies:
-        Policy 1. Applies time masking with a given probability.
-        Policy 2. Applies frequency masking with a given probability.
-        Policy 3. Applies both time masking and frequency masking with the same probability.
-    """
-
-    def __init__(self, rate, policy=3, freq_mask=30, time_mask=100):
-        super(SpecAugment, self).__init__()
-
-        self.rate = rate
-
-        self.specaug = nn.Sequential(
-            torchaudio.transforms.FrequencyMasking(freq_mask_param=freq_mask),
-            torchaudio.transforms.TimeMasking(time_mask_param=time_mask)
-        )
-
-        self.specaug2 = nn.Sequential(
-            torchaudio.transforms.FrequencyMasking(freq_mask_param=freq_mask),
-            torchaudio.transforms.TimeMasking(time_mask_param=time_mask),
-            torchaudio.transforms.FrequencyMasking(freq_mask_param=freq_mask),
-            torchaudio.transforms.TimeMasking(time_mask_param=time_mask)
-        )
-
-        policies = { 1: self.policy1, 2: self.policy2, 3: self.policy3 }
-        self._forward = policies[policy]
-
-    def forward(self, x):
-        return self._forward(x)
-
-    def policy1(self, x):
-        probability = torch.rand(1, 1).item()
-        if self.rate > probability:
-            return  self.specaug(x)
-        return x
-
-    def policy2(self, x):
-        probability = torch.rand(1, 1).item()
-        if self.rate > probability:
-            return  self.specaug2(x)
-        return x
-
-    def policy3(self, x):
-        probability = torch.rand(1, 1).item()
-        if probability > 0.5:
-            return self.policy1(x)
-        return self.policy2(x)
+import torchaudio.transforms as transforms
+from torch.utils.data import DataLoader, Dataset
+from utils import TextTransform
 
 
 class LogMelSpec(nn.Module):
-    """
-    Args:
-        sample_rate (int): The sample rate of the audio signal.
-        n_mels (int): The number of mel filters.
-        win_length (int): The window length in milliseconds.
-        hop_length (int): The hop length in milliseconds.
-
-    Methods:
-        - forward(x): Applies the Mel Spectrogram transformation to the input tensor.
-    """
-    def __init__(self, sample_rate=16000, n_mels=128, win_length=160, hop_length=80):
+    def __init__(self, sample_rate=16000, n_mels=80, win_length=400, hop_length=160, n_fft=512):
         super(LogMelSpec, self).__init__()
-        self.transform = torchaudio.transforms.MelSpectrogram(
-                            sample_rate=sample_rate, n_mels=n_mels,
-                            win_length=win_length, hop_length=hop_length)
+        self.transform = transforms.MelSpectrogram(sample_rate=sample_rate, 
+                                                   n_mels=n_mels,
+                                                   win_length=win_length,
+                                                   hop_length=hop_length, 
+                                                   n_fft = n_fft)
 
     def forward(self, x):
-        x = self.transform(x)  # mel spectrogram
-        x = np.log(x + 1e-14)  # logrithmic, add small value to avoid inf
+        x = self.transform(x)
+        # x = torch.log(x + 1e-14)  # logarithmic, add small value to avoid inf
         return x
 
 
-def get_featurizer(sample_rate, n_feats=81):
-    return LogMelSpec(sample_rate=sample_rate, n_mels=n_feats,  win_length=160, hop_length=80)
+def get_featurizer(sample_rate=16000, n_mels=80, win_length=400, hop_length=160, n_fft=512):
+    return LogMelSpec(sample_rate=sample_rate, 
+                      n_mels=n_mels,
+                      win_length=win_length,
+                      hop_length=hop_length, 
+                      n_fft = n_fft)
 
 
-class Data(torch.utils.data.Dataset):
-    """
-    Parameters:
-        - json_path (str): Path to the JSON file containing audio file information.
-        - sample_rate (int): Sample rate of the audio data.
-        - n_feats (int): Number of mel spectrogram features.
-        - specaug_rate (float): Rate of applying SpecAugment data augmentation.
-        - specaug_policy (int): Policy for SpecAugment augmentation (1, 2, or 3).
-        - time_mask (int): Maximum time masking parameter for SpecAugment.
-        - freq_mask (int): Maximum frequency masking parameter for SpecAugment.
-        - valid (bool): If True, the dataset is for validation (no data augmentation).
-        - shuffle (bool): If True, shuffle the dataset.
-        - text_to_int (bool): If True, convert text labels to integer sequences.
-        - log_ex (bool): If True, log exceptions during data loading.
-
-    Methods:
-        - __len__(): Returns the number of samples in the dataset.
-        - __getitem__(idx): Retrieves the specified sample from the dataset.
-
-    Usage Example:
-        data = Data(json_path='data.json', sample_rate=8000, n_feats=81, specaug_rate=0.5,
-                    specaug_policy=3, time_mask=70, freq_mask=15, valid=False, shuffle=True)
-    """
-
-    # this makes it easier to be overide in argparse
-    parameters = {
-        "sample_rate": 8000, 
-        "n_feats": 81,
-        "specaug_rate": 0.5, 
-        "specaug_policy": 3,
-        "time_mask": 100, 
-        "freq_mask": 30 
-    }
-
-    def __init__(self, json_path, sample_rate, n_feats, specaug_rate, specaug_policy,
-                time_mask, freq_mask, valid=False, shuffle=True, text_to_int=True, log_ex=True):
+class CustomAudioDataset(Dataset):
+    def __init__(self, dataset, transform=None, log_ex=True, valid=False):
+        self.dataset = dataset
+        self.text_process = TextTransform()  # Initialize TextProcess for text processing
         self.log_ex = log_ex
-        self.text_process = TextProcess()
-
-        print("Loading data json file from", json_path)
-        self.data = pd.read_json(json_path, encoding="utf-8")
-
-        # print(self.data[1])
 
         if valid:
             self.audio_transforms = torch.nn.Sequential(
-                LogMelSpec(sample_rate=sample_rate, n_mels=n_feats,  win_length=160, hop_length=80)
+                LogMelSpec()
             )
         else:
             self.audio_transforms = torch.nn.Sequential(
-                LogMelSpec(sample_rate=sample_rate, n_mels=n_feats,  win_length=160, hop_length=80),
-                SpecAugment(specaug_rate, specaug_policy, freq_mask, time_mask)
+                LogMelSpec(),
+                transforms.FrequencyMasking(freq_mask_param=30),
+                transforms.TimeMasking(time_mask_param=70)
             )
 
-
     def __len__(self):
-        return len(self.data)
+        return len(self.dataset)
 
     def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.item()
-
         try:
-            file_path = self.data.key.iloc[idx]
-            waveform, _ = torchaudio.load(file_path)
-            label = self.text_process.text_to_int_sequence(self.data['text'].iloc[idx])
-            spectrogram = self.audio_transforms(waveform) # (channel, feature, time)
+            waveform, _, utterance, _, _, _ = self.dataset[idx]
+            utterance = utterance.lower()
+            label = self.text_process.text_to_int(utterance)
+
+            # Apply audio transformations
+            spectrogram = self.audio_transforms(waveform)  # (channel, feature, time)
+
             spec_len = spectrogram.shape[-1] // 2
             label_len = len(label)
-            if spec_len < label_len:
-                raise Exception('spectrogram len is bigger then label len')
-            if spectrogram.shape[0] > 1:
-                raise Exception('dual channel, skipping audio file %s'%file_path)
-            if spectrogram.shape[2] > 8000:
-                raise Exception('spectrogram to big. size %s'%spectrogram.shape[2])
-            if label_len == 0:
-                raise Exception('label len is zero... skipping %s'%file_path)
+
+            # Check if spectrogram or label length is valid
+            if spec_len < label_len or spectrogram.shape[0] > 1 or label_len == 0:
+                raise ValueError('Invalid spectrogram or label length.')
+
+            return spectrogram, label, spec_len, label_len
+
         except Exception as e:
             if self.log_ex:
-                print(str(e), file_path)
-            return self.__getitem__(idx - 1 if idx != 0 else idx + 1)  
-        return spectrogram, label, spec_len, label_len
+                print(f"{str(e)}\r", end='')
+            return self.__getitem__(idx - 1 if idx != 0 else idx + 1)
 
-    def describe(self):
-        return self.data.describe()
 
-def collate_fn_padd(data):
-    """
-    Padds batch of variable length
+class SpeechDataModule(pl.LightningDataModule):
+    def __init__(self, batch_size, train_url, test_url, num_workers):
+        super().__init__()
+        self.batch_size = batch_size
+        self.train_url = train_url
+        self.test_url = test_url
+        self.num_workers = num_workers
+        self.text_process = TextTransform() 
 
-    note: it converts things ToTensor manually here since the ToTensor transform
-    assume it takes in images rather than arbitrary tensors.
-    """
+    def setup(self, stage=None):
+        # Load multiple training and test URLs
+        train_dataset = [torchaudio.datasets.LIBRISPEECH("./data", url=url, download=True) for url in self.train_url]
+        test_dataset = [torchaudio.datasets.LIBRISPEECH("./data", url=url, download=True) for url in self.test_url]
+
+        # Concatenate multiple datasets into one
+        combined_train_dataset = torch.utils.data.ConcatDataset(train_dataset)
+        combined_test_dataset = torch.utils.data.ConcatDataset(test_dataset)
+
+        self.train_dataset = CustomAudioDataset(combined_train_dataset, valid=False)
+        self.test_dataset = CustomAudioDataset(combined_test_dataset, valid=True)
+
+    def data_processing(self, data):
+        spectrograms, labels, input_lengths, label_lengths = [], [], [], []
+        for (spectrogram, label, input_length, label_length) in data:
+            if spectrogram is None:
+                continue
+            spectrograms.append(spectrogram.squeeze(0).transpose(0, 1))
+            labels.append(torch.Tensor(label))
+            input_lengths.append(input_length)
+            label_lengths.append(label_length)
+
+        # Pad the spectrograms to have the same width (time dimension)
+        spectrograms = nn.utils.rnn.pad_sequence(spectrograms, batch_first=True).unsqueeze(1).transpose(2, 3)
+        labels = nn.utils.rnn.pad_sequence(labels, batch_first=True)
+
+        # Convert input_lengths and label_lengths to tensors
+        input_lengths = torch.tensor(input_lengths, dtype=torch.long)
+        label_lengths = torch.tensor(label_lengths, dtype=torch.long)
+
+
+        return spectrograms, labels, input_lengths, label_lengths
+        
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset,
+                          batch_size=self.batch_size,
+                          shuffle=True,
+                          collate_fn=self.data_processing,
+                          num_workers=self.num_workers,
+                          pin_memory=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.test_dataset,
+                          batch_size=self.batch_size,
+                          shuffle=False,
+                          collate_fn=self.data_processing,
+                          num_workers=self.num_workers,
+                          pin_memory=True)
     
-    # print(data)
-    # delay[1000]
-    spectrograms = []
-    labels = []
-    input_lengths = []
-    label_lengths = []
-    # print (data[1])
-    for (spectrogram, label, input_length, label_length) in data:
-        if spectrogram is None:
-            continue
-        # print(spectrogram.shape)
-
-        spectrograms.append(spectrogram.squeeze(0).transpose(0, 1))
-        labels.append(torch.Tensor(label))
-        input_lengths.append(input_length)
-        label_lengths.append(label_length)
-
-    spectrograms = nn.utils.rnn.pad_sequence(spectrograms, batch_first=True).unsqueeze(1).transpose(2, 3)
-    labels = nn.utils.rnn.pad_sequence(labels, batch_first=True)
-    input_lengths = input_lengths
-    # print(spectrograms.shape)
-    label_lengths = label_lengths
-    # ## compute mask
-    # mask = (batch != 0).cuda(gpu)
-    # return batch, lengths, mask
-    return spectrograms, labels, input_lengths, label_lengths
--- neuralnet/utils.py
+++ neuralnet/utils.py
@@ -1,115 +1,85 @@
 import torch
 
-class TextProcess:
-	"""
-    Class to handle text processing for speech recognition.
-    """
-	def __init__(self):
-		"""
-        Initializes the TextProcess class with character map and index map.
+class TextTransform:
+    """ Maps characters to integers and vice versa """
+    def __init__(self):
+        char_map_str = """
+        ' 0
+        <SPACE> 1
+        a 2
+        b 3
+        c 4
+        d 5
+        e 6
+        f 7
+        g 8
+        h 9
+        i 10
+        j 11
+        k 12
+        l 13
+        m 14
+        n 15
+        o 16
+        p 17
+        q 18
+        r 19
+        s 20
+        t 21
+        u 22
+        v 23
+        w 24
+        x 25
+        y 26
+        z 27
         """
-		char_map_str = """
-		' 0
-		<SPACE> 1
-		a 2
-		b 3
-		c 4
-		d 5
-		e 6
-		f 7
-		g 8
-		h 9
-		i 10
-		j 11
-		k 12
-		l 13
-		m 14
-		n 15
-		o 16
-		p 17
-		q 18
-		r 19
-		s 20
-		t 21
-		u 22
-		v 23
-		w 24
-		x 25
-		y 26
-		z 27
-		"""
-		self.char_map = {}
-		self.index_map = {}
-		for line in char_map_str.strip().split('\n'):
-			ch, index = line.split()
-			self.char_map[ch] = int(index)
-			self.index_map[int(index)] = ch
-		self.index_map[1] = ' '
+        self.char_map = {}
+        self.index_map = {}
+        for line in char_map_str.strip().split('\n'):
+            ch, index = line.split()
+            self.char_map[ch] = int(index)
+            self.index_map[int(index)] = ch
+        self.index_map[1] = ' '
 
-	def text_to_int_sequence(self, text):
-		"""
-		Map the characters and convert text to an integer sequence
 
-        Args:
-            text (str): Input text to be converted.
+    def text_to_int(self, text):
+        """ Use a character map and convert text to an integer sequence """
+        int_sequence = []
+        special_chars = {' ', '–', '.', '-'}     # Some special chars 
+        for c in text:
+            if c in special_chars:
+                ch = self.char_map['<SPACE>']
+            else:
+                ch = self.char_map[c]
+                # print('Mapped Chars:', ch)
+            int_sequence.append(ch)
+        return int_sequence
 
-        Returns:
-            list: Integer sequence representing the input text.
-        """
-		
-		int_sequence = []
-		for c in text:
-			if c == ' ' or '.' or ',':
-				ch = self.char_map['<SPACE>']
-			else:
-				ch = self.char_map[c]
-			int_sequence.append(ch)
-		return int_sequence
 
-	def int_to_text_sequence(self, labels):
-		"""
-        Convert integer labels to a text sequence using a character map.
-
-        Args:
-            labels (list): List of integer labels to be converted.
-
-        Returns:
-            str: Text sequence representing the integer labels.
-        """
-		string = []
-		for i in labels:
-			string.append(self.index_map[i])
-		return ''.join(string).replace('<SPACE>', ' ')
-
+    def int_to_text(self, labels):
+        """ Use a character map and convert integer labels to a text sequence """
+        string = []
+        for i in labels:
+            string.append(self.index_map[i]) 
+            # print(string[0])
+        return ''.join(string).replace('<SPACE>', ' ')
+    
 # Initialize TextProcess for text processing
-textprocess = TextProcess()
+text_transform = TextTransform()
 
-# NOTE: GreedyDecoder function for decoding model output using a greedy approach.
 def GreedyDecoder(output, labels, label_lengths, blank_label=28, collapse_repeated=True):
-	"""
-    Perform greedy decoding to convert model output to text sequences.
-
-    Args:
-        output (torch.Tensor): Model output tensor.
-        labels (torch.Tensor): Ground truth labels tensor.
-        label_lengths (list): List of label lengths.
-        blank_label (int): Index of the blank label (default: 28).
-        collapse_repeated (bool): Whether to collapse repeated characters (default: True).
-
-    Returns:
-        tuple: Tuple containing decoded text sequences and ground truth targets.
-    """
-	arg_maxes = torch.argmax(output, dim=2)
-	decodes = []
-	targets = []
-	for i, args in enumerate(arg_maxes):
-		decode = []
-		targets.append(textprocess.int_to_text_sequence(
-				labels[i][:label_lengths[i]].tolist()))
-		for j, index in enumerate(args):
-			if index != blank_label:
-				if collapse_repeated and j != 0 and index == args[j -1]:
-					continue
-				decode.append(index.item())
-		decodes.append(textprocess.int_to_text_sequence(decode))
-	return decodes, targets
+    # print(output)
+    arg_maxes = torch.argmax(output, dim=2)
+    # print('Argmax indices: ', arg_maxes[0]) 
+    decodes = []
+    targets = []
+    for i, args in enumerate(arg_maxes):
+        decode = []
+        targets.append(text_transform.int_to_text(labels[i][:label_lengths[i]].tolist()))
+        for j, index in enumerate(args):
+            if index != blank_label:
+                if collapse_repeated and j != 0 and index == args[j-1]:
+                    continue
+                decode.append(index.item())
+        decodes.append(text_transform.int_to_text(decode))
+    return decodes, targets
